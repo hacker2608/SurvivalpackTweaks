ee77d95ea6bc16a3c29d72c54fe0080a1262d0df
hacker2608 <max.pfistner@gmx.de>
2015-08-30 20:28:12 +0200

    framework

---
+++ scr/main/java/com/hacker2608/SurvivalpackTweaks/SurvivalpackTweaks.java
@@ -0,0 +1,34 @@
+package com.hacker2608.SurvivalpackTweaks;
+
+import com.hacker2608.SurvivalpackTweaks.proxy.IProxy;
+import com.hacker2608.SurvivalpackTweaks.reference.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_ID, version = Reference.VERSION)
+public class SurvivalpackTweaks {
+
+    @Mod.Instance(Reference.MOD_ID)
+    public static SurvivalpackTweaks instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY, serverSide = Reference.SERVER_PROXY)
+    public static IProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
---
+++ scr/main/java/com/hacker2608/SurvivalpackTweaks/reference/Reference.java
@@ -0,0 +1,12 @@
+package com.hacker2608.SurvivalpackTweaks.reference;
+
+import java.util.SplittableRandom;
+
+public class Reference {
+
+    public static final String MOD_ID = "SurvivalpackTweaks";
+    public static final String VERSION = "beta5";
+    public static final String CLIENT_PROXY = "com.hacker2608.SurvivalpackTweaks.proxy.ClientProxy";
+    public static final String SERVER_PROXY = "com.hacker2608.SurvivalpackTweaks.proxy.ServerProxy";
+
+}
