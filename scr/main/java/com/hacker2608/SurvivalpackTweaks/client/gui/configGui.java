b2040a780731b89cb4bf02d3fa83cc6ac3e58da2
hacker2608 <max-pfistner@gmx.de>
2015-08-31 13:00:23 +0200

    update

--- scr/main/java/com/hacker2608/SurvivalpackTweaks/client/gui/configGui.java
+++ scr/main/java/com/hacker2608/SurvivalpackTweaks/client/gui/configGui.java
@@ -1,4 +1,21 @@
 package com.hacker2608.SurvivalpackTweaks.client.gui;
 
-public class configGui {
+import com.hacker2608.SurvivalpackTweaks.handler.configHandler;
+import com.hacker2608.SurvivalpackTweaks.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class configGui extends GuiConfig {
+
+    public configGui(GuiScreen screen) {
+        super(  screen ,
+                new ConfigElement(configHandler.config.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(configHandler.config.toString())
+        );
+    }
 }
